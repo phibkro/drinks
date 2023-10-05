75e1c38fa3c8051d1103940e42c2d9423ddcd9ea
phibkro <philib.krogh@gmail.com>
2023-10-05 15:34:33 +0200

    feat: :sparkles: add theme toggling

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,11 +1,14 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import "./index.css";
+import "./globals.css";
 import { appRouter } from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={appRouter} />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={appRouter} />
+    </ThemeProvider>
   </React.StrictMode>
 );
