00012f47c4510db8bf414cb90d250799cf6682e3
Eskild Oeveren <eskildov@stud.ntnu.no>
2023-10-10 13:24:59 +0200

    feat: :sparkles: let user pick rating

--- app/src/components/ReviewForm.tsx
+++ app/src/components/ReviewForm.tsx
@@ -1,18 +1,30 @@
 import { SubmitHandler, useForm } from "react-hook-form";
 import { Martini } from "lucide-react";
+import { useState } from "react";
 
 export type ReviewData = {
-  comment: string;
+  /*
+  Type for data submitted through the form
+  */
+  reviewId: number;
   rating: number;
+  comment: string;
 };
 
+export const ratedColor = "#eb8634";
+
+let martiniGlasses: number[] = [1, 2, 3, 4, 5];
+
 function ReviewForm() {
+  const [rating, setRating] = useState(0); //Set 0 as default rating
+
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
   } = useForm<ReviewData>();
+
   const onSubmit: SubmitHandler<ReviewData> = (data) => console.log(data);
 
   return (
@@ -20,20 +32,39 @@ function ReviewForm() {
       <div className="flex flex-col border-2 w-[40em] h-[20em] justify-center items-center">
         <p className="text-xl">Give this cocktail a review!</p>
         <div className="flex flex-row my-[1em]">
-          <Martini />
-          <Martini />
-          <Martini />
-          <Martini />
-          <Martini />
+          {martiniGlasses.map((glass) =>
+            glass <= rating ? (
+              <Martini
+                color={ratedColor}
+                onClick={() => setRating(glass)}
+                className="hover:cursor-pointer"
+                key={glass}
+              />
+            ) : (
+              <Martini
+                onClick={() => setRating(glass)}
+                className="hover:cursor-pointer"
+                key={glass}
+              />
+            ),
+          )}
         </div>
         <textarea
           placeholder="Did you like your cocktail?"
-          {...register("comment")}
+          {...register("comment", { minLength: 1 })}
           rows={4}
           cols={50}
           className="bg-gray-50 rounded text-black my-[0.5em] px-[0.2em]"
         ></textarea>
-        <input type="submit" className="border-2 h-[2em] w-[5em] my-[0.5em]" />
+        {errors.comment && (
+          <span className=" text-black my-[0.5em] px-[0.2em]">
+            Please add a comment before submitting you review
+          </span>
+        )}
+        <input
+          type="submit"
+          className="border-2 h-[2em] w-[5em] my-[0.5em] hover:cursor-pointer hover:border-white"
+        />
       </div>
     </form>
   );
