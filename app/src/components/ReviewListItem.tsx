1094ad47bb369047b7f0b3b14e88fa0f23b52bfb
Eskild Oeveren <eskildov@stud.ntnu.no>
2023-10-10 13:24:06 +0200

    feat :sparkles: display diffrence in rating

--- app/src/components/ReviewListItem.tsx
+++ app/src/components/ReviewListItem.tsx
@@ -1,16 +1,28 @@
 import React from "react";
 import { ReviewData } from "./ReviewForm";
 import { Martini } from "lucide-react";
+import { ratedColor } from "./ReviewForm";
 
-function ReviewListItem({ comment, rating }: ReviewData) {
+let martiniGlasses: number[] = [1, 2, 3, 4, 5];
+
+function ReviewListItem(
+  { comment, rating, reviewId }: ReviewData,
+  key: number,
+) {
   return (
     <div className="flex flex-col my-[0.5em] w-[29em]">
       <div className="flex flex-row justify-center my-[0.5em]">
-        <Martini />
-        <Martini />
-        <Martini />
-        <Martini />
-        <Martini />
+        {martiniGlasses.map((glass) =>
+          glass <= rating ? (
+            <Martini
+              color={ratedColor}
+              className="hover:cursor-pointer"
+              key={glass}
+            />
+          ) : (
+            <Martini className="hover:cursor-pointer" key={glass} />
+          ),
+        )}
       </div>
       <div>
         <p>{comment}</p>
