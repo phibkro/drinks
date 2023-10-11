e5495cebcfb1e0ec1a8b3f26cc45b8183232e97c
Philip Bjørknes Krogh <philipbk@stud.ntnu.no>
2023-10-11 19:07:39 +0200

    refactor: :truck: move Item components to their user

--- app/src/components/ResultList.tsx
+++ app/src/components/ResultList.tsx
@@ -1,5 +1,5 @@
 import { DrinkDetails } from "@/data/types";
-import { ResultListItem } from "./ResultListItem";
+import { Link } from "react-router-dom";
 
 interface Result {
   results: DrinkDetails[];
@@ -11,4 +11,32 @@ export function ResultList(props: Result) {
     listTest.push(ResultListItem(item));
   });
   return <div className="flex flex-col gap-5">{listTest}</div>;
+}
+
+export function ResultListItem({
+  strDrink: name,
+  strDrinkThumb: image,
+  ingredients,
+}: DrinkDetails) {
+  const renderList = () => {
+    const listItems = [];
+    for (let i = 0; i < ingredients.length; i++) {
+      listItems.push(<li key={i}>{ingredients[i]}</li>);
+    }
+    return listItems;
+  };
+  return (
+    <Link to={`details/${name}`}>
+      <div className="flex gap-4 bg-primary-foreground hover:cursor-pointer">
+        <div className="basis-1/4">
+          <img src={image} alt={`Image of ${name}`} />
+        </div>
+        <h2>{name}</h2>
+        <div>
+          <h3>Ingredients</h3>
+          <ul>{renderList()}</ul>
+        </div>
+      </div>
+    </Link>
+  );
 }
--- app/src/components/ReviewList.tsx
+++ app/src/components/ReviewList.tsx
@@ -1,7 +1,8 @@
 import * as reviewData from "@/data/mockReviews.json";
-import ReviewListItem from "./ReviewListItem";
+import { Martini } from "lucide-react";
+import { ratedColor } from "./ReviewForm";
 
-function ReviewList({
+export default function ReviewList({
   drinkId,
   className,
 }: {
@@ -26,4 +27,27 @@ function ReviewList({
   );
 }
 
-export default ReviewList;
+const martiniGlasses: number[] = [1, 2, 3, 4, 5];
+
+export function ReviewListItem(review: { comment: string; rating: number }) {
+  return (
+    <div className="my-[0.5em] flex w-[29em] flex-col border-t-2 py-[0.5em]">
+      <div className="my-[0.5em] flex flex-row justify-center">
+        {martiniGlasses.map((glass) =>
+          glass <= review.rating ? (
+            <Martini
+              color={ratedColor}
+              className="hover:cursor-pointer"
+              key={glass}
+            />
+          ) : (
+            <Martini className="hover:cursor-pointer" key={glass} />
+          ),
+        )}
+      </div>
+      <div>
+        <p>{review.comment}</p>
+      </div>
+    </div>
+  );
+}
