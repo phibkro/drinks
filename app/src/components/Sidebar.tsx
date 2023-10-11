8d980e9174494812cdc6cc703be94e548e46ff0a
Philip Bjørknes Krogh <philipbk@stud.ntnu.no>
2023-10-11 19:45:10 +0200

    style: :art: remove useless divs

--- app/src/components/Sidebar.tsx
+++ app/src/components/Sidebar.tsx
@@ -6,50 +6,47 @@ import { ratedColor } from "./ReviewForm";
 
 export function Sidebar() {
   return (
-    <div>
-      <div>
-        <h2 className="text-xl">Sorting</h2>
-        <RadioGroup defaultValue="option-one">
-          <Label className="text-lg" htmlFor="option-one">
-            <RadioGroupItem value="option-one" id="option-one" />
-            A-Z
-          </Label>
-          <Label className="text-lg" htmlFor="option-two">
-            <RadioGroupItem value="option-two" id="option-two" />
-            Z-A
-          </Label>
-        </RadioGroup>
+    <div className="flex flex-col gap-2">
+      <h2 className="text-xl">Sorting</h2>
+      <RadioGroup defaultValue="option-one" className="flex flex-col">
+        <Label className="flex items-center gap-1 text-lg" htmlFor="option-one">
+          <RadioGroupItem value="option-one" id="option-one" />
+          A-Z
+        </Label>
+        <Label className="flex items-center gap-1 text-lg" htmlFor="option-two">
+          <RadioGroupItem value="option-two" id="option-two" />
+          Z-A
+        </Label>
+      </RadioGroup>
+
+      <h2 className="text-xl">Alcohol</h2>
+      <div className="flex">
+        <Checkbox />
+        <label
+          htmlFor="terms1"
+          className="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+        >
+          Non-alcoholic
+        </label>
       </div>
-      <div>
-        <h2 className="text-xl">Alcohol</h2>
-        <div className="flex">
+
+      <h2 className="text-xl">Rating</h2>
+      {[...Array(5).keys()].map((n) => (
+        <div className="flex" key={n}>
           <Checkbox />
           <label
             htmlFor="terms1"
-            className="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-          >
-            Non-alcoholic
-          </label>
+            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          ></label>
+          {[...Array(n + 1).keys()].map((m) => (
+            <Martini
+              color={ratedColor}
+              className="hover:cursor-pointer"
+              key={m}
+            />
+          ))}
         </div>
-
-        <h2 className="text-xl">Rating</h2>
-        {[...Array(5).keys()].map((n) => (
-          <div className="flex" key={n}>
-            <Checkbox />
-            <label
-              htmlFor="terms1"
-              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-            ></label>
-            {[...Array(n + 1).keys()].map((m) => (
-              <Martini
-                color={ratedColor}
-                className="hover:cursor-pointer"
-                key={m}
-              />
-            ))}
-          </div>
-        ))}
-      </div>
+      ))}
     </div>
   );
 }
