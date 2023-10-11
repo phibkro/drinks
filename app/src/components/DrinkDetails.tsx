ce6f1a2bf39c08a9ef9b1e159674bacd6660f8e2
Philip Bjørknes Krogh <philipbk@stud.ntnu.no>
2023-10-11 17:04:18 +0200

    refactor: :recycle: extract drinkDetails type

--- app/src/components/DrinkDetails.tsx
+++ app/src/components/DrinkDetails.tsx
@@ -1,3 +1,4 @@
+import { DrinkDetails } from "@/data/types";
 /* interface DrinkDetails {
   id: number;
   name: string;
@@ -12,57 +13,7 @@
   imageAttribution: string;
   creativeCommonsConfirmed: string;
 } */
-export interface DrinkDetails {
-  idDrink: string;
-  strDrink: string;
-  strDrinkAlternate?: string | null;
-  strTags?: string | null;
-  strVideo?: string | null;
-  strCategory?: string | null;
-  strIBA?: string | null;
-  strAlcoholic?: string;
-  strGlass?: string | null;
-  strInstructions: string;
-  strInstructionsES?: string | null;
-  strInstructionsDE?: string | null;
-  strInstructionsFR?: string | null;
-  strInstructionsIT?: string | null;
-  strDrinkThumb: string;
-  strIngredient1?: string | null;
-  strIngredient2?: string | null;
-  strIngredient3?: string | null;
-  strIngredient4?: string | null;
-  strIngredient5?: string | null;
-  strIngredient6?: string | null;
-  strIngredient7?: string | null;
-  strIngredient8?: string | null;
-  strIngredient9?: string | null;
-  strIngredient10?: string | null;
-  strIngredient11?: string | null;
-  strIngredient12?: string | null;
-  strIngredient13?: string | null;
-  strIngredient14?: string | null;
-  strIngredient15?: string | null;
-  strMeasure1?: string | null;
-  strMeasure2?: string | null;
-  strMeasure3?: string | null;
-  strMeasure4?: string | null;
-  strMeasure5?: string | null;
-  strMeasure6?: string | null;
-  strMeasure7?: string | null;
-  strMeasure8?: string | null;
-  strMeasure9?: string | null;
-  strMeasure10?: string | null;
-  strMeasure11?: string | null;
-  strMeasure12?: string | null;
-  strMeasure13?: string | null;
-  strMeasure14?: string | null;
-  strMeasure15?: string | null;
-  strImageSource: string | null;
-  strImageAttribution: string | null;
-  strCreativeCommonsConfirmed: string;
-  dateModified: string | null;
-}
+
 interface Props extends DrinkDetails {}
 export default function DrinkDetails({
   strDrink: name,
@@ -137,7 +88,7 @@ export default function DrinkDetails({
   ].filter((value) => value !== null);
   // ^ transforming multiple individual variables to arrays without null values
   return (
-    <div className="flex gap-8 flex-wrap">
+    <div className="flex flex-wrap gap-8">
       <header className="basis-full text-left">
         <h1 className="text-4xl">{name}</h1>
       </header>
