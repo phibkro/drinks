0f5541096e9c6912b6e68129faac5110022b0615
Philip Bjørknes Krogh <philipbk@stud.ntnu.no>
2023-11-04 15:08:56 +0100

    feat(server): :sparkles: add csvToJSON cli program

--- server/src/lib/utils.ts
+++ server/src/lib/utils.ts
@@ -1,21 +1,4 @@
-import { readFile, writeFile } from "fs/promises";
-
-async function main() {
-  if (process.argv.length < 3) {
-    throw new Error("Please provide a file path");
-  }
-  if (process.argv[2].endsWith(".csv") === false) {
-    throw new Error("Please provide a csv file");
-  }
-  const filePath = process.argv[2];
-  // const filePath = "data/cleaned_all_drinks.csv";
-  const data = await readFile(filePath, "utf8");
-  CSVCheck(data, ";");
-}
-
-main();
-
-function CSVCheck(data: string, delimiter: string) {
+export function csvValidate(data: string, delimiter: string) {
   const lines = data.split("\n");
   // id,strDrink,dateModified,idDrink,strAlcoholic,strCategory,strDrinkThumb,strGlass,strIBA,strIngredient1,strIngredient10,strIngredient11,strIngredient12,strIngredient13,strIngredient14,strIngredient15,strIngredient2,strIngredient3,strIngredient4,strIngredient5,strIngredient6,strIngredient7,strIngredient8,strIngredient9,strInstructions,strMeasure1,strMeasure10,strMeasure11,strMeasure12,strMeasure13,strMeasure14,strMeasure15,strMeasure2,strMeasure3,strMeasure4,strMeasure5,strMeasure6,strMeasure7,strMeasure8,strMeasure9,strVideo
   const columnTitles = lines[0].split(delimiter);
@@ -34,4 +17,23 @@ function CSVCheck(data: string, delimiter: string) {
   }
   console.log(wrongLines);
   console.log(wrongCount);
+  if (wrongCount > 0) {
+    throw new Error("There are wrong lines in the csv file");
+  }
+}
+
+export function csvToJson(csv: string) {
+  const lines = csv.split("\n");
+  const header = lines[0].split(";").map((column) => column.trim());
+  const body = lines.slice(1);
+
+  const items = body.map((line) => {
+    const columns = line.split(";");
+    const item: any = {};
+    header.forEach((column, index) => {
+      item[column] = columns[index];
+    });
+    return item;
+  });
+  return items;
 }
