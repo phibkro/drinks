17edc84f43f157cfdd7f0c383dba15fe46eca450
Philip Bjørknes Krogh <philipbk@stud.ntnu.no>
2023-11-04 20:58:30 +0100

    feat(server): :seedling: add ingredients & measures seed

--- server/src/prisma/seed.ts
+++ server/src/prisma/seed.ts
@@ -1,12 +1,17 @@
 import { PrismaClient } from "@prisma/client";
-import ingredientData from "../data/ingredients.json" assert { type: "json" };
+import seedIngredients from "../data/seedIngredients.json" assert { type: "json" };
 import seedDrinks from "../data/seedDrinks.json" assert { type: "json" };
+import seedMeasures from "../data/seedMeasures.json" assert { type: "json" };
 
 const prisma = new PrismaClient();
 async function main() {
   // Populate db
-  await prisma.ingredient.createMany(ingredientData);
+  await prisma.ingredient.createMany(seedIngredients);
   await prisma.drink.createMany(seedDrinks);
+  await prisma.measure.createMany({
+    data: seedMeasures.data,
+    skipDuplicates: true,
+  });
 }
 main()
   .catch(async (e) => {
