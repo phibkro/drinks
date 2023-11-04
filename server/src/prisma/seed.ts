b010100cb5049e3ed53fe65f96fa15daea395a9b
Philip Bjørknes Krogh <philipbk@stud.ntnu.no>
2023-11-04 15:45:13 +0100

    refactor(server): :truck: move seed.ts

--- server/src/prisma/seed.ts
+++ server/src/prisma/seed.ts
@@ -1,9 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import ingredientData from "../data/ingredients.json" assert { type: "json" };
+import seedDrinks from "../data/seedDrinks.json" assert { type: "json" };
 
 const prisma = new PrismaClient();
 async function main() {
+  // Populate db
   await prisma.ingredient.createMany(ingredientData);
+  await prisma.drink.createMany(seedDrinks);
 }
 main()
   .then(async () => {
