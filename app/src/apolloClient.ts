f96cbc50220cd6c81547db2b56e61757cf8c10f4
Philip Bjørknes Krogh <philipbk@stud.ntnu.no>
2023-11-05 15:17:45 +0100

    feat(app): :heavy_plus_sign: setup apolloClient provider

---
+++ app/src/apolloClient.ts
@@ -0,0 +1,7 @@
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+
+export const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
+
