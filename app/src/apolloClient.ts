abf1642322ca003e7ce411900fcef9ec192ee0f0
solvhold <solvhold@stud.ntnu.no>
2023-11-08 14:41:46 +0100

    feat: :construction: add typePolicies
    #41

--- app/src/apolloClient.ts
+++ app/src/apolloClient.ts
@@ -5,5 +5,19 @@ export const client = new ApolloClient({
   uri: "http://localhost:4000",
   // Use when on NTNU vm
   // uri: "http://it2810-40.idi.ntnu.no:4000/",
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          searchDrinksByName:{
+            keyArgs: false,
+            merge(existing = [], incoming) {
+              return [...existing, ...incoming];
+            },
+          }
+          
+        }
+      }
+    }
+  }),
 });
