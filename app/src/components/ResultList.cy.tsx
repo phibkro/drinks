db6e5f971299d41edb30ba3e84a408f071101a3c
solvhold <solvhold@stud.ntnu.no>
2023-11-18 22:45:44 +0100

    test: :white_check_mark: add component test for resultlist, so far only code for one of two resultlistitems
    test that element exist, visisble and have det correct text (data passed to the component) with mock data
    
    #50

---
+++ app/src/components/ResultList.cy.tsx
@@ -0,0 +1,74 @@
+import { MemoryRouter } from "react-router-dom";
+import { ResultList } from "./ResultList";
+
+/// <reference types="Cypress"/>
+
+const drinkData1 = {
+  id: 1,
+  name: "Test Drink",
+  instructions: "Hello world",
+  alcoholic: true,
+  imageUrl: "",
+  glass: "Vinglass",
+  measures: [
+    { measure: "2 oz", ingredient: { name: "Ingredient 1" } },
+    { measure: "1 oz", ingredient: { name: "Ingredient 2" } },
+  ],
+};
+const drinkData2 = {
+  id: 1,
+  name: "Test Drink 2",
+  instructions: "Hello world 2",
+  alcoholic: true,
+  imageUrl: "",
+  glass: "Vinglass 2",
+  measures: [
+    { measure: "4 oz", ingredient: { name: "Ingredient 1" } },
+    { measure: "6 oz", ingredient: { name: "Ingredient 2" } },
+  ],
+};
+
+describe("DrinkDetails Component", () => {
+  beforeEach(() => {
+    cy.mount(
+      <MemoryRouter>
+        <ResultList results={[drinkData1, drinkData2]} />
+      </MemoryRouter>,
+    );
+  });
+
+  it("should display list of ResultListItems with passed data", () => {
+    // testing first ResultListItem
+    cy.get(".flex-col > :nth-child(1) > .flex h2")
+      .should("exist")
+      .and("be.visible")
+      .and("have.text", drinkData1.name);
+
+    cy.get(".flex-col > :nth-child(1) > .flex")
+      .find("img")
+      .should("be.visible")
+      .and("have.attr", "src", drinkData1.imageUrl);
+
+    cy.get(".flex-col > :nth-child(1) > .flex h3")
+      .should("exist")
+      .and("be.visible")
+      .and("have.text", "Ingredients");
+
+    cy.get(".flex-col > :nth-child(1) > .flex ul li")
+      .should("exist")
+      .and("be.visible")
+      .and("have.length", 2);
+
+    cy.get(".flex-col > :nth-child(1) > .flex ul li:eq(1)")
+      .should("exist")
+      .and("be.visible")
+      .and(
+        "have.text",
+        drinkData1.measures[1].measure +
+          " " +
+          drinkData1.measures[1].ingredient.name,
+      );
+
+    cy.get(":nth-child(2) > .flex");
+  });
+});
