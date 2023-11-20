ec8ac35d0b7ea3547e8968219eefd6b5ebd64f60
Philip Bjørknes Krogh <philipbk@stud.ntnu.no>
2023-11-20 14:44:51 +0100

    fix(app): :bug: change to hashRouter for vm routing
    Change to hashRouter as browserRouter fucks with vm routing. This means that the routes now include a # after root.
    
    Now using hashRouter instead of browserRouter

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,14 @@
 import { HomeIcon } from "lucide-react";
 import { Link, Outlet } from "react-router-dom";
 import { ModeToggle } from "./components/ui/ModeToggle";
+import { rootPath } from "./router";
 
 function App() {
   return (
     <div className="flex min-h-screen flex-col gap-5 p-8">
       <header>
         <nav className="flex justify-between">
-          <Link to={import.meta.env.BASE_URL}>
+          <Link to={rootPath}>
             <HomeIcon />
           </Link>
           <ModeToggle />
