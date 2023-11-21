b3b7d53787a66f99ec16c6d13c9b5d60334ae513
solvhold <solvhold@stud.ntnu.no>
2023-11-21 23:44:35 +0100

    test: :white_check_mark: test sorting functionality
    test both endpoint and e2e for sorting
    
    #69

---
+++ app/cypress/e2e/searchPage/Sorting.cy.ts
@@ -0,0 +1,78 @@
+
+
+describe("Sorting", () =>{
+    beforeEach(() => {
+        cy.visit('http://localhost:5173/project2');
+   });
+
+  it('should call qraphgl query with sort asc', () => {
+    cy.get('#option-one').click()
+    cy.intercept('POST', 'http://localhost:4000/').as('backendIterceptAsc')
+    cy.get('main.flex > :nth-child(1) > :nth-child(1) > .inline-flex').click()
+    cy.wait('@backendIterceptAsc').its('request.body.variables.options.sort').should('contain', 'asc')
+
+  });
+
+  it('should call qraphgl query with sort desc', () => {
+    cy.get('#option-two').click()
+    cy.intercept('POST', 'http://localhost:4000/').as('backendIterceptDesc') // have to intercept after sort value changed
+    cy.get('main.flex > :nth-child(1) > :nth-child(1) > .inline-flex').click()
+    
+    cy.wait('@backendIterceptDesc').its('request.body.variables.options.sort').should('contain', 'desc')
+
+  })
+
+  it('should test backend with desc', () => {
+    cy.request('POST', 'http://localhost:4000/', {
+       operationName: 'SearchDrinksByName',
+       query: "query SearchDrinksByName($name: String!, $options: SearchOptions, $offset: Int, $limit: Int) {\n  searchDrinksByName(\n    name: $name\n    options: $options\n    offset: $offset\n    limit: $limit\n  ) {\n    id\n    name\n    alcoholic\n    glass\n    instructions\n    imageUrl\n    measures {\n      measure\n      ingredient {\n        name\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}",
+       variables: {
+        limit: 10,
+        name: "",
+        offset: 0,
+        options: {
+            alcohol: true,
+            sort: 'desc'
+        }
+       } 
+    }).then((res) => {
+        const resultNameList: string[] = [];
+        
+        res.body.data.searchDrinksByName.forEach((element: any) => {
+            resultNameList.push(element.name)
+        });
+
+        let sortedDesc = resultNameList.slice().sort((a, b) => a.localeCompare(b)).reverse()        
+        cy.wrap(resultNameList).should('deep.equal', sortedDesc)
+        
+        
+    })
+  })
+
+  it('should test backend with asc', () => {
+    cy.request('POST', 'http://localhost:4000/', {
+       operationName: 'SearchDrinksByName',
+       query: "query SearchDrinksByName($name: String!, $options: SearchOptions, $offset: Int, $limit: Int) {\n  searchDrinksByName(\n    name: $name\n    options: $options\n    offset: $offset\n    limit: $limit\n  ) {\n    id\n    name\n    alcoholic\n    glass\n    instructions\n    imageUrl\n    measures {\n      measure\n      ingredient {\n        name\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}",
+       variables: {
+        limit: 10,
+        name: "",
+        offset: 0,
+        options: {
+            alcohol: true,
+            sort: 'asc'
+        }
+       } 
+    }).then((res) => {
+        const resultNameList: string[] = [];
+        
+        res.body.data.searchDrinksByName.forEach((element: any) => {
+            resultNameList.push(element.name)
+        });
+
+        let sortedDesc = resultNameList.slice().sort((a, b) => a.localeCompare(b))        
+        cy.wrap(resultNameList).should('deep.equal', sortedDesc)
+        
+        
+    })
+  })
+})
