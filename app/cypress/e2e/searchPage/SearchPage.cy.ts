2293fe8bc7aded6f02d571b378ba6b19b2fea871
solvhold <solvhold@stud.ntnu.no>
2023-11-21 12:52:41 +0100

    test: :white_check_mark: add e2e test for searchbar
    #69

--- app/cypress/e2e/searchPage/SearchPage.cy.ts
+++ app/cypress/e2e/searchPage/SearchPage.cy.ts
@@ -1,7 +1,12 @@
+/// <reference types="Cypress"/>
+
+
 describe("SearchPage Component", () => {
   beforeEach(() => {
-    cy.visit("/"); // replace with the actual path to your SearchPage component
+    cy.visit("http://localhost:5173/project2"); // replace with the actual path to your SearchPage component
   });
+
+  // testing search functionality
 
   it("should display results on page load", () => {
     cy.get("[data-cy=result-list]").should("be.visible"); // replace 'search-result' with the actual id of the element you want to test
@@ -12,11 +17,36 @@ describe("SearchPage Component", () => {
   });
 
   it("should allow typing in the search input", () => {
-    cy.get("input[type=text]").type("Test search");
+    const search = "Margarita"
+    cy.get("input[type=text]").type(search).should("have.value", search);
+
+    cy.get('input[type="text"]').type('{enter}');
+
+    cy.get('[data-cy=result-list]').should('be.visible');
   });
 
   it("should display results when a search is performed", () => {
     cy.get("input[type=text]").type("marg{enter}");
-    cy.get("[data-cy=result-list]").should("be.visible"); // replace 'search-result' with the actual id of the element you want to test
+    cy.get("[data-cy=result-list]").should("be.visible"); 
+    cy.get('input[type=text]').clear().should('have.value', '');
   });
+
+  it("should go back to initial state by clearing search bar ", () => {
+    cy.get('input[type=text]').clear().should('have.value', '');
+  });
+
+  it("search is ordered alphabetically",() => {
+    // i dont know how to test this
+  });
+
+  // testing resultlist functionality
+
+  it("should ", () => {
+
+
+  })
+
+  it("", () => {
+    
+  })
 });
