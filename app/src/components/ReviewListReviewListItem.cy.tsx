17352d95d10c9a2bce3cff7475081d8a3e912b84
Eskild Oeveren <eskildov@stud.ntnu.no>
2023-11-21 14:15:16 +0100

    test(app): :white_check_mark: update reviewListItem test to support new reviewListItem

--- app/src/components/ReviewListReviewListItem.cy.tsx
+++ app/src/components/ReviewListReviewListItem.cy.tsx
@@ -4,7 +4,7 @@ describe("<ReviewListItem />", () => {
   it("renders with correct rating and comment", () => {
     cy.mount(<ReviewListItem comment={"Good enough for testing"} rating={3} />);
     cy.contains("Good enough for testing");
-    cy.get("[data-cy=ratedMartini]").should("have.length", 3);
-    cy.get("[data-cy=unratedMartini]").should("have.length", 2);
+    cy.get("svg").should("have.length", 5);
+    cy.get("svg[stroke=white]").should("have.length", 2);
   });
 });
