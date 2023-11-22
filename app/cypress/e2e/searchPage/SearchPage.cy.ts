8e1ee1457725e40d80655332a8db4a7a6b2b61ab
solvhold <solvhold@stud.ntnu.no>
2023-11-22 19:28:29 +0100

    fix: :white_check_mark: fix cy.get to get correct element with new classnames

--- app/cypress/e2e/searchPage/SearchPage.cy.ts
+++ app/cypress/e2e/searchPage/SearchPage.cy.ts
@@ -3,49 +3,45 @@
 
 describe("SearchPage Component", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:5173/project2"); // replace with the actual path to your SearchPage component
+    cy.visit("http://localhost:5173/project2"); // replace with the path to website when running
   });
 
-  // testing search functionality
-
+  // testing search
   it("should display results on page load", () => {
-    cy.get("[data-cy=result-list]").should("be.visible"); // replace 'search-result' with the actual id of the element you want to test
+    cy.get("[data-cy=result-list]").should("be.visible"); 
   });
 
   it("should display a search input", () => {
-    cy.get("input[type=text]").should("be.visible");
+    cy.get('.leading-none > .flex').should("be.visible");
   });
 
   it("should allow typing in the search input", () => {
     const search = "Margarita"
-    cy.get("input[type=text]").type(search).should("have.value", search);
-
-    cy.get('input[type="text"]').type('{enter}');
-
+    cy.get('.leading-none > .flex').type(search).should("have.value", search);
+    cy.get('.leading-none > .flex').type('{enter}');
     cy.get('[data-cy=result-list]').should('be.visible');
   });
 
   it("should display results when a search is performed", () => {
-    cy.get("input[type=text]").type("marg{enter}");
+    cy.get('.leading-none > .flex').type("marg{enter}");
     cy.get("[data-cy=result-list]").should("be.visible"); 
-    cy.get('input[type=text]').clear().should('have.value', '');
+    cy.get('.leading-none > .flex').clear().should('have.value', '');
   });
 
   it("should go back to initial state by clearing search bar ", () => {
-    cy.get('input[type=text]').clear().should('have.value', '');
+    cy.get('.leading-none > .flex').clear().should('have.value', '');
   });
 
+  // test routing to drinkdetails
   it("should navigate to drinkdetails",() => {
-    cy.get('[href="/project2/details/0"] > [data-cy="result-list-items"]').click()
+    cy.get('[href="#/details/0"] > [data-cy="result-list-items"]').click()
     cy.url().should('include', 'details/0')
   });
 
+  // testing pagination
   it("should load more elements when load more button is clicked", () => {
     cy.get('[data-cy="result-list-items"]').should('have.length', 10)
-    cy.get('.basis-3\\/4 > .inline-flex').click()
+    cy.get('.gap-10 > :nth-child(3)').click()
     cy.get('[data-cy="result-list-items"]').should('have.length', 20)
   })
-
-  // testing resultlist functionality
-
 });
