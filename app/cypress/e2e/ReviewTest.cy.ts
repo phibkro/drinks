beb884bc485e43b8647b7d1fe1f5096c7ac04945
Eskild Oeveren <eskildov@stud.ntnu.no>
2023-11-22 15:02:47 +0100

    test(app): :test_tube: start making test for reviewPosting

--- app/cypress/e2e/ReviewTest.cy.ts
+++ app/cypress/e2e/ReviewTest.cy.ts
@@ -1,6 +1,36 @@
+import RemoveReviewButton from "./RemoveReviewButton"
+import { client} from gql
+
+export const hasOperationName = (req, operationName: string) => {
+  const { body } = req
+  return (
+    Object.prototype.hasOwnProperty.call(body, 'operationName') &&
+    body.operationName === operationName
+  )
+}
+
+// Alias query if operationName matches
+export const aliasQuery = (req, operationName: string) => {
+  if (hasOperationName(req, operationName)) {
+    req.alias = `gql${operationName}Query`
+  }
+}
+
+// Alias mutation if operationName matches
+export const aliasMutation = (req, operationName: string) => {
+  if (hasOperationName(req, operationName)) {
+    req.alias = `gql${operationName}Mutation`
+  }
+}
+
+
+// components/somewhere.ts   
 describe("API SPY", () => {
   beforeEach(() => {
-    cy.visit("/details/8"); // replace with the actual path to your SearchPage component
+    cy.visit("/details/8");
+    cy.intercept("POST", "http://localhost:4000/", (req) => {
+      aliasMutation(req, "RemoveReview")
+    })
   });
   it("post review", () => {
     cy.intercept("POST", "http://localhost:4000/" ).as("newReview")
@@ -11,8 +41,16 @@ describe("API SPY", () => {
     cy.get("textarea").click().type("Booooozy. And goooood")
     cy.get("input").click()
 
-
-    cy.wait("@newReview").its("response.statusCode").should("eq", 200)
-
+    cy.wait("@gqlRemoveReviewMutation").should(nop => {
+      console.log("mop");
+      console.log(nop);
+      
+    })
+  
+    cy.get("@newReview").should(newReview => {
+      console.log("Response");
+      console.log(newReview.response.body);
+      
+    })
   })
 })
