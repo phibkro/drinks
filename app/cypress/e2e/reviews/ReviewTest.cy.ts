12baa14da85eadbb927b951e57220344c77171cb
Eskild Oeveren <eskildov@stud.ntnu.no>
2023-11-26 14:49:03 +0100

    test(app): :white_check_mark: test if added review in e2e reveiw test is in ReviewList after submitting

--- app/cypress/e2e/reviews/ReviewTest.cy.ts
+++ app/cypress/e2e/reviews/ReviewTest.cy.ts
@@ -6,28 +6,30 @@ describe("Review Submitting", () => {
   });
 
   it("Loads both components", () => {
-    //Gjør skriving
+    cy.get("form").should("exist")
+    cy.get("[data-cy=reviewList]").should("exist")
     })
-    it("Submitted review appears in ReviewList without reloading", () => {
-      //Må oppdateres til nye ReviewList og form
-      cy.get("button[role=radio]").eq(4).click()
-      cy.get("textarea").click().type("Booooozy. And goooood")
-      cy.get("button[type=submit]").click()
-      cy.get("textarea").should("not.exist")
-      cy.get("[data-cy=reviewListItem]").last().should("contain.text", "Booooozy. And goooood")
-    })
+  it("Submitted review appears in ReviewList without reloading", () => {
+    cy.get("[data-cy=reviewListItem]")
+    cy.get("button[role=radio]").eq(3).click()
+    cy.get("textarea").click().type("Booooozy. And goooood")
+    cy.get("button[type=submit]").click()
+    cy.get("textarea").should("not.exist")
+    cy.get("[data-cy=reviewListItem]").last().should("contain.text", "Booooozy. And goooood")
+  })
 
-    it("Both reviews are in ReviewList after a new one is loaded", () => {
-      cy.get("button[role=radio]").eq(4).click()
-      cy.get("textarea").click().type("Booooozy. And goooood")
-      cy.get("button[type=submit]").click()
-      cy.get("textarea").should("not.exist")
-      cy.get("[data-cy=reviewListItem]").last().should("contain.text", "Booooozy. And goooood")
-    })
-    it("Both reviews are in ReviewList after re-visiting the drink", () => {})
-    it("Comment and rating in submitted review is the same as the one in ReviewList", () => {})
-    it("Loads both components", () => {})
-    it("Loads both components", () => {})
-    it("Loads both components", () => {})
-    it("Loads both components", () => {})
+  it("Both reviews are in ReviewList after a new one is loaded", () => {
+    cy.get("button[role=radio]").eq(3).click()
+    cy.get("textarea").click().type("Booooozy. And goooood")
+    cy.get("button[type=submit]").click()
+    cy.get("textarea").should("not.exist")
+    cy.get("[data-cy=reviewListItem]").last().should("contain.text", "Booooozy. And goooood")
+     })
+
+  it("Comment and rating in submitted review is the same as the one in ReviewList", () => {
+    cy.get("[data-cy=reviewListItem]").last().find("svg").should("have.length", 5)
+    cy.get("[data-cy=reviewListItem]").last().find("[data-cy=ratedMartini]").should("have.length", 4)
+    cy.get("[data-cy=reviewListItem]").last().find("[data-cy=unratedMartini]").should("have.length", 1)
+    cy.get("[data-cy=reviewListItem]").last().find("p").should("have.text", "Booooozy. And goooood")
+  })
   })
--- app/src/components/ReviewList.tsx
+++ app/src/components/ReviewList.tsx
@@ -16,6 +16,7 @@ export default function ReviewList({
   return (
     <div
       className={`flex w-[40em] flex-col items-center justify-center border-2 ${className}`}
+      data-cy="reviewList"
     >
       <p className="my-4 text-xl">Did other people like this cocktail?</p>
       {reviews.map((review) => (
@@ -33,10 +34,18 @@ const ratingRange: number[] = [1, 2, 3, 4, 5];
 
 export function ReviewListItem(review: { comment: string; rating: number }) {
   return (
-    <div className="my-[0.5em] flex w-[29em] flex-col border-t-2 py-[0.5em]" data-cy="reviewListItem">
+    <div
+      className="my-[0.5em] flex w-[29em] flex-col border-t-2 py-[0.5em]"
+      data-cy="reviewListItem"
+    >
       <div className="my-[0.5em] flex flex-row justify-center">
         {ratingRange.map((i) => (
-          <Martini aria-label="Martini glass icon" color={i <= review.rating ? ratedColor : "white"} key={i} />
+          <Martini
+            aria-label="Martini glass icon"
+            color={i <= review.rating ? ratedColor : "white"}
+            data-cy={i <= review.rating ? "ratedMartini" : "unratedMartini"}
+            key={i}
+          />
         ))}
       </div>
       <p>{review.comment}</p>
