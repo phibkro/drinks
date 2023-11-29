bdda239021ee62d8b95561c78c9753d82540e0c6
Philip Bjørknes Krogh <philipbk@stud.ntnu.no>
2023-11-29 19:59:14 +0100

    style(app): :bulb: update comments

--- app/src/apolloClient.ts
+++ app/src/apolloClient.ts
@@ -2,9 +2,9 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { offsetLimitPagination } from "@apollo/client/utilities";
 
 export const client = new ApolloClient({
-  // Use when running locally
+  // Connect to local server
   uri: "http://localhost:4000",
-  // Use when on NTNU vm
+  // Connect to PROD on vm
   // uri: "http://it2810-40.idi.ntnu.no:4000/",
   cache: new InMemoryCache({
     typePolicies: {
