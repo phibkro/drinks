c8e8aafccdecd7314b8adbc2553027564295b4da
Philip Bjørknes Krogh <philipbk@stud.ntnu.no>
2023-11-29 16:08:03 +0100

    refactor(app): :art: change search limit to 12 to accomodate styling

--- app/src/pages/SearchPage.tsx
+++ app/src/pages/SearchPage.tsx
@@ -23,7 +23,7 @@ export default function SearchPage() {
         name: inputVar(),
         options: optionsVar(),
         offset: 0,
-        limit: 10,
+        limit: 12,
       },
     },
   );
