b030033511858a5d7547fbc87d8c743a907ff1c9
Philip Bjørknes Krogh <philipbk@stud.ntnu.no>
2024-09-20 17:53:11 +0200

    style(server): :art: format with biome

--- server/src/index.ts
+++ server/src/index.ts
@@ -75,125 +75,125 @@ const typeDefs = `#graphql
 // Resolvers define how to fetch the types defined in your schema.
 // This resolver retrieves books from the "books" array above.
 const resolvers = {
-  // TODO: Query ingredients by drink by
-  // getting all ingredients for measures in a drink
-  Query: {
-    allDrinks: () => prisma.drink.findMany(),
-    drinkById: (_parent, args) =>
-      prisma.drink.findUnique({
-        where: {
-          id: args.id,
-        },
-      }),
-    searchDrinksByName: (_parent, args) => {
-      return prisma.drink.findMany({
-        skip: args.offset,
-        take: args.limit,
-        orderBy: {
-          name: args.options?.sort,
-        },
-        where: {
-          alcoholic: args.options?.alcohol,
-          name: {
-            contains: args.name,
-            mode: "insensitive",
-          },
-        },
-      });
-    },
-    allIngredients: () => prisma.ingredient.findMany(),
-    ingredientById: (_parent, args) =>
-      prisma.ingredient.findUnique({ where: { id: args.id } }),
+	// TODO: Query ingredients by drink by
+	// getting all ingredients for measures in a drink
+	Query: {
+		allDrinks: () => prisma.drink.findMany(),
+		drinkById: (_parent, args) =>
+			prisma.drink.findUnique({
+				where: {
+					id: args.id,
+				},
+			}),
+		searchDrinksByName: (_parent, args) => {
+			return prisma.drink.findMany({
+				skip: args.offset,
+				take: args.limit,
+				orderBy: {
+					name: args.options?.sort,
+				},
+				where: {
+					alcoholic: args.options?.alcohol,
+					name: {
+						contains: args.name,
+						mode: "insensitive",
+					},
+				},
+			});
+		},
+		allIngredients: () => prisma.ingredient.findMany(),
+		ingredientById: (_parent, args) =>
+			prisma.ingredient.findUnique({ where: { id: args.id } }),
 
-    allReviews: () => prisma.review.findMany(),
-    reviewsByDrinkId: (_parent, args) =>
-      prisma.review.findMany({
-        where: {
-          drink: {
-            id: args.id,
-          },
-        },
-      }),
-    reviewById: (_parent, args) =>
-      prisma.review.findUnique({
-        where: {
-          id: args.id,
-        },
-      }),
-    allMeasures: () => prisma.measure.findMany(),
-    measuresInDrink: (_parent, args) => {
-      return prisma.measure.findMany({
-        where: {
-          drink: {
-            id: args.id,
-          },
-        },
-      });
-    },
-  },
-  Mutation: {
-    addReview: (_parent, args) =>
-      prisma.review.create({
-        data: {
-          drink: {
-            connect: {
-              id: args.drinkId,
-            },
-          },
-          rating: args.rating,
-          textContent: args.textContent,
-        },
-      }),
-      removeReview: (_parent, args) =>
-        prisma.review.delete({
-          where: {
-            id: args.id
-          }
-        })
-  },
+		allReviews: () => prisma.review.findMany(),
+		reviewsByDrinkId: (_parent, args) =>
+			prisma.review.findMany({
+				where: {
+					drink: {
+						id: args.id,
+					},
+				},
+			}),
+		reviewById: (_parent, args) =>
+			prisma.review.findUnique({
+				where: {
+					id: args.id,
+				},
+			}),
+		allMeasures: () => prisma.measure.findMany(),
+		measuresInDrink: (_parent, args) => {
+			return prisma.measure.findMany({
+				where: {
+					drink: {
+						id: args.id,
+					},
+				},
+			});
+		},
+	},
+	Mutation: {
+		addReview: (_parent, args) =>
+			prisma.review.create({
+				data: {
+					drink: {
+						connect: {
+							id: args.drinkId,
+						},
+					},
+					rating: args.rating,
+					textContent: args.textContent,
+				},
+			}),
+		removeReview: (_parent, args) =>
+			prisma.review.delete({
+				where: {
+					id: args.id,
+				},
+			}),
+	},
 
-  // Trivial resolvers for relations
-  Drink: {
-    // Resolve the 'measures' field for the Drink type
-    measures: (parent) =>
-      prisma.measure.findMany({ where: { drinkId: parent.id } }),
-    reviews: (parent) =>
-      prisma.review.findMany({ where: { drinkId: parent.id } }),
-  },
-  Ingredient: {
-    // Resolve the 'measure' field for the Ingredient type
-    measure: (parent) =>
-      prisma.measure.findMany({ where: { ingredientId: parent.id } }),
-  },
-  Measure: {
-    // Resolve the 'ingredient' field for the Measure type
-    ingredient: (parent) =>
-      prisma.ingredient.findUnique({ where: { id: parent.ingredientId } }),
+	// Trivial resolvers for relations
+	Drink: {
+		// Resolve the 'measures' field for the Drink type
+		measures: (parent) =>
+			prisma.measure.findMany({ where: { drinkId: parent.id } }),
+		reviews: (parent) =>
+			prisma.review.findMany({ where: { drinkId: parent.id } }),
+	},
+	Ingredient: {
+		// Resolve the 'measure' field for the Ingredient type
+		measure: (parent) =>
+			prisma.measure.findMany({ where: { ingredientId: parent.id } }),
+	},
+	Measure: {
+		// Resolve the 'ingredient' field for the Measure type
+		ingredient: (parent) =>
+			prisma.ingredient.findUnique({ where: { id: parent.ingredientId } }),
 
-    // Resolve the 'drink' field for the Measure type
-    drink: (parent) =>
-      prisma.drink.findUnique({ where: { id: parent.drinkId } }),
-  },
-  Review: {
-    drink: (parent) =>
-      prisma.drink.findUnique({ where: { id: parent.drinkId } }),
-  },
+		// Resolve the 'drink' field for the Measure type
+		drink: (parent) =>
+			prisma.drink.findUnique({ where: { id: parent.drinkId } }),
+	},
+	Review: {
+		drink: (parent) =>
+			prisma.drink.findUnique({ where: { id: parent.drinkId } }),
+	},
 };
 
 const environment = process.env.NODE_ENV || "development";
 
 const server =
-  environment === "production"
-    ? new ApolloServer({
-        typeDefs,
-        resolvers,
-      })
-    : new ApolloServer({
-        schema: addMocksToSchema({
-          schema: makeExecutableSchema({ typeDefs, resolvers }),
-          preserveResolvers: true,
-        }),
-      });
+	environment === "production"
+		? new ApolloServer({
+				typeDefs,
+				resolvers,
+			})
+		: new ApolloServer({
+				schema: addMocksToSchema({
+					schema: makeExecutableSchema({ typeDefs, resolvers }),
+					preserveResolvers: true,
+				}),
+			});
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
@@ -203,7 +203,7 @@ const server =
 //  2. installs your ApolloServer instance as middleware
 //  3. prepares your app to handle incoming requests
 const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
+	listen: { port: 4000 },
 });
 
 console.log(`🚀  Server ready at: ${url}`);
--- server/src/prisma/seed.ts
+++ server/src/prisma/seed.ts
@@ -1,23 +1,25 @@
 import { PrismaClient } from "@prisma/client";
-import seedIngredients from "../data/seedIngredients.json" assert { type: "json" };
+import seedIngredients from "../data/seedIngredients.json" assert {
+	type: "json",
+};
 import seedDrinks from "../data/seedDrinks.json" assert { type: "json" };
 import seedMeasures from "../data/seedMeasures.json" assert { type: "json" };
 
 const prisma = new PrismaClient();
 async function main() {
-  // Populate db
-  await prisma.ingredient.createMany(seedIngredients);
-  await prisma.drink.createMany(seedDrinks);
-  await prisma.measure.createMany({
-    data: seedMeasures.data,
-    skipDuplicates: true,
-  });
+	// Populate db
+	await prisma.ingredient.createMany(seedIngredients);
+	await prisma.drink.createMany(seedDrinks);
+	await prisma.measure.createMany({
+		data: seedMeasures.data,
+		skipDuplicates: true,
+	});
 }
 main()
-  .catch(async (e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+	.catch(async (e) => {
+		console.error(e);
+		process.exit(1);
+	})
+	.finally(async () => {
+		await prisma.$disconnect();
+	});
